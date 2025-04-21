95a460627824b51562fa98db67fda0ea28566e4c
VVKnowsStuff <vitor.giacomelli@hotmail.com>
2025-04-21 08:44:49 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,5 @@
-export default (req, res) => res.redirect(307, 'https://your-ngrok.ngrok-free.app' + req.url);
+export default function handler(req, res) {
+  const newBase = "https://f18c-209-14-239-77.ngrok-free.app"; // your current ngrok URL
+  const fullPath = `${newBase}${req.url}`;
+  res.redirect(307, fullPath);
+}
